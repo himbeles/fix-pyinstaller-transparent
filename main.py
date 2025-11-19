0110c46a4fb9ea7e6dbbc71de2bfc3b514a8af85
Luis Riegger <lri@me.com>
2025-11-19 08:30:37 +0100

    update example

--- main.py
+++ main.py
@@ -3,21 +3,12 @@ from multiprocessing import freeze_support
 
 freeze_support()  # noqa
 
-from nicegui import app, ui
+from nicegui import app, native, ui
 
 app.native.window_args['transparent'] = True
 app.native.window_args['frameless'] = True
 
+def root():
+    ui.label('Hello from PyInstaller')
 
-def main():
-    with ui.card():
-        ui.label('Hello, NiceGUI!')
-
-    ui.run(
-        reload=False,
-        native=True,
-    )
-
-
-if __name__ in ('__main__', '__mp_main__'):
-    main()
+ui.run(root, reload=False, native=True, port=native.find_open_port())
